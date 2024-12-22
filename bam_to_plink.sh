ba08397a05c7995c2afaa7b40f63e8fd3297d835
teepean <tnatkinn@gmail.com>
2024-12-22 14:17:22 +0200

    Update bam_to_plink.sh

--- bam_to_plink.sh
+++ bam_to_plink.sh
@@ -1,97 +1,116 @@
 #!/bin/bash
 
-# Enable case-insensitive pattern matching
-shopt -s nocasematch
+# Check for required programs
+for prog in samtools pileupCaller; do
+    if ! command -v $prog &> /dev/null; then
+        echo "Error: $prog is not installed or not in PATH"
+        echo "Please install $prog and try again"
+        exit 1
+    fi
+done
 
-# Check if hs37d5.fa exists in the reference directory
-if [ ! -f reference/hs37d5.fa ]; then
-  echo "You need to have hs37d5.fa in the reference directory."
-  read -p "Do you want to download it? (y/n) " yn
-  case $yn in
-    [Yy]* )
-      echo "Downloading..."
-      cd reference
-      curl --insecure -O https://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/phase2_reference_assembly_sequence/hs37d5.fa.gz
-      echo "Decompressing..."
-      bgzip -d -@ 2 hs37d5.fa.gz
-      echo "Indexing hs37d5.fa..."
-      samtools faidx hs37d5.fa
-      cd ..
-      ;;
-    * ) ;;
-  esac
+# Check for hs37d5.fa
+if [ ! -f "reference/hs37d5.fa" ]; then
+    echo
+    echo "You need to have hs37d5.fa in the reference directory"
+    echo "Do you want to download it? (y/n)"
+    read -n 1 answer
+    echo
+    
+    if [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
+        echo "Downloading"
+        cd reference
+        curl --insecure -O https://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/phase2_reference_assembly_sequence/hs37d5.fa.gz
+        echo "Decompressing"
+        bgzip -d -@ 2 hs37d5.fa.gz
+        echo "Indexing hs37d5.fa"
+        samtools faidx hs37d5.fa
+        cd ..
+    else
+        exit 0
+    fi
+    echo
 fi
 
-# Check if hg19.fa exists in the reference directory
-if [ ! -f reference/hg19.fa ]; then
-  echo "You need to have hg19.fa in the reference directory."
-  read -p "Do you want to download it? (y/n) " yn
-  case $yn in
-    [Yy]* )
-      echo "Downloading..."
-      cd reference
-      curl --insecure -O https://hgdownload.cse.ucsc.edu/goldenpath/hg19/bigZips/hg19.fa.gz
-      echo "Decompressing..."
-      bgzip -d -@ 2 hg19.fa.gz
-      echo "Indexing hg19.fa..."
-      samtools faidx hg19.fa
-      cd ..
-      ;;
-    * ) ;;
-  esac
+# Check for hg19.fa
+if [ ! -f "reference/hg19.fa" ]; then
+    echo
+    echo "You need to have hg19.fa in the reference directory"
+    echo "Do you want to download it? (y/n)"
+    read -n 1 answer
+    echo
+    
+    if [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
+        echo "Downloading"
+        cd reference
+        curl --insecure -O https://hgdownload.cse.ucsc.edu/goldenpath/hg19/bigZips/hg19.fa.gz
+        echo "Decompressing"
+        bgzip -d -@ 2 hg19.fa.gz
+        echo "Indexing hg19.fa"
+        samtools faidx hg19.fa
+        cd ..
+    else
+        exit 0
+    fi
+    echo
 fi
 
-# Prompt user for Population name
+# Prompt for population name and output name
 read -p "Enter Population name: " POPNAME
-
-# Prompt user for Output name
 read -p "Enter Output name: " OUTPUTNAME
 
-# Navigate to the source directory and list all .bam and .cram files
+# Navigate to source directory
 cd source
-# Enable nullglob to ensure unmatched patterns are ignored
-shopt -s nullglob
 
+# Initialize variables
+headerCheck=""
+FLAG="NEITHER"
 bamlist1=""
-headerCheck="false"
 
+# Process BAM/CRAM files
 for file in *.bam *.cram; do
-  if [ "$headerCheck" = "false" ]; then
-    headerCheck="true"
-    HEADER_TEMP="header_temp.txt"
-    samtools view -H "$file" > "$HEADER_TEMP"
-    FLAG="NEITHER"
-
-    if grep -q -b -f ../patternhs37d5.txt "$HEADER_TEMP"; then
-      FLAG="HS37D5"
+    # Skip if no files found
+    [[ -e "$file" ]] || continue
+    
+    bamlist1="$bamlist1 $file"
+    
+    # Process header for the first file only
+    if [ -z "$headerCheck" ]; then
+        headerCheck=$file
+        HEADER_TEMP="header_temp.txt"
+        samtools view -H "$headerCheck" > "$HEADER_TEMP"
+        
+        # Check patterns
+        if grep -f ../patternhs37d5.txt "$HEADER_TEMP" > /dev/null; then
+            FLAG="HS37D5"
+        elif grep -f ../patternhg19.txt "$HEADER_TEMP" > /dev/null; then
+            FLAG="HG19"
+        fi
+        rm "$HEADER_TEMP"
     fi
-    if grep -q -b -f ../patternhg19.txt "$HEADER_TEMP"; then
-      FLAG="HG19"
-    fi
-    rm "$HEADER_TEMP"
-  fi
-  bamlist1="$bamlist1 $file"
 done
 
-# Disable nullglob if you don't want it to affect subsequent scripts or commands
-shopt -u nullglob
-
-array2=()
-
-for i in $bamlist1; do
-  name=$(basename "$i" .bam)
-  array2+=("$name")
+# Create sample names list
+array2=""
+for file in $bamlist1; do
+    name=$(basename "$file" | sed 's/\.[^.]*$//')
+    array2="${array2},${name}"
 done
+array2_string=${array2:1}  # Remove first comma
 
-array2_string=$(IFS=,; echo "${array2[*]}")
-
-if [ "$FLAG" == "HS37D5" ]; then
-  echo "Running command"
-  samtools mpileup -B -q 30 -Q 30 -l ../positions/v42.4.1240K.pos -f ../reference/hs37d5.fa $bamlist1 | pileupCaller --randomHaploid --sampleNames "$array2_string" --samplePopName "$POPNAME" -f ../positions/v42.4.1240K.snp -p ../target/"$OUTPUTNAME"
-elif [ "$FLAG" == "HG19" ]; then
-  echo "Running command"
-  samtools mpileup -B -q 30 -Q 30 -l ../positions/v42.4.hg19.pos -f ../reference/hg19.fa $bamlist1 | sed "s/chr//" | pileupCaller --randomHaploid --sampleNames "$array2_string" --samplePopName "$POPNAME" -f ../positions/v42.4.1240K.snp -p ../target/"$OUTPUTNAME"
+# Run appropriate command based on reference type
+if [ "$FLAG" = "HS37D5" ]; then
+    echo "Running command"
+    samtools mpileup -B -q 30 -Q 30 -l ../positions/v42.4.1240K.pos -f ../reference/hs37d5.fa $bamlist1 | \
+    pileupCaller --majorityCall --sampleNames "$array2_string" --samplePopName "$POPNAME" \
+    -f ../positions/v42.4.1240K.snp -p "../target/$OUTPUTNAME" > "../target/$OUTPUTNAME.stats.txt" 2>&1
+elif [ "$FLAG" = "HG19" ]; then
+    echo "Running command"
+    samtools mpileup -B -q 30 -Q 30 -l ../positions/v42.4.hg19.pos -f ../reference/hg19.fa $bamlist1 | \
+    sed "s/chr//" | \
+    pileupCaller --majorityCall --sampleNames "$array2_string" --samplePopName "$POPNAME" \
+    -f ../positions/v42.4.1240K.snp -p "../target/$OUTPUTNAME" > "../target/$OUTPUTNAME.stats.txt" 2>&1
 else
-  echo "Reference is not compatible"
-  read -p "Press enter to continue..."
+    echo "Reference is not compatible"
+    exit 1
 fi
